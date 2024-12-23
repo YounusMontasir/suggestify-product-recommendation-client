b9e12c427b9abbe9e92aa62e000380ac94f3f39e
YounusMontasir <ymontasir73@gmail.com>
2024-12-23 17:18:19 +0600

    delete query done

--- src/components/MyQuery.jsx
+++ src/components/MyQuery.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../provider/AuthProvider';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const MyQuery = () => {
     const [myQuery, setMyQuery] = useState([])
@@ -15,19 +16,35 @@ const MyQuery = () => {
 
         
     })
-    const {
-        _id,
-        productName,
-        productBrand,
-        productImageURL,
-        queryTitle,
-        boycottingReason,
-        userEmail,
-        userName,
-        userProfileImage,
-        currentDateTime,
-        recommendationCount
-      } = myQuery
+    const handleDeleteQuery = (id) =>{
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`http://localhost:5000/queries/${id}`,{
+            method: 'DELETE'
+          })
+          .then(res=>res.json())
+          .then(data=>{
+            if(data.deletedCount>0){
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+              const exist = myQuery.filter(query=> query._id !== id)
+              setMyQuery(exist)
+            }
+          })
+        }
+      });
+    }
     
     return (
         <div className='grid grid-cols-3 w-10/12 mx-auto mt-20 mb-24'>
@@ -46,7 +63,7 @@ const MyQuery = () => {
     <div className="card-actions">
      <Link to={`/queryupdate/${query._id}`}><button className='btn'>Update</button></Link>
      <Link to={`/querydetails/${query._id}`}><button class="btn btn-primary">Details</button></Link>
-     <Link><button className='btn'>Delete</button></Link>
+     <Link><button onClick={()=>handleDeleteQuery(query._id)} className='btn'>Delete</button></Link>
     </div>
   </div>
 </div>
