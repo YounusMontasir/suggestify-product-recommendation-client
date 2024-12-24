d7b80966ef5acc5c236797bf0f228f0dc3108b40
YounusMontasir <ymontasir73@gmail.com>
2024-12-24 07:30:11 +0600

    get the data of my recommendations and Recommendations section

--- src/pages/MyRecommendation.jsx
+++ src/pages/MyRecommendation.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../provider/AuthProvider';
 
 const MyRecommendation = () => {
+    const {user} = useContext(AuthContext)
+    const [myRecommendations, setMyRecommendations] = useState([])
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/recommendations?recommenderEmail=${user.email}`)
+        .then(res=>{
+           setMyRecommendations(res.data)
+            
+        })
+    },[user?.email])
     return (
         <div>
-            dssd
+            {myRecommendations.length}
         </div>
     );
 };
--- src/pages/RecommendationForYou.jsx
+++ src/pages/RecommendationForYou.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../provider/AuthProvider';
+import axios from 'axios';
 
 const RecommendationForYou = () => {
+    const {user} = useContext(AuthContext)
+    const [recommendations, setRecommendations] = useState([])
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/recommendations?userEmail=${user.email}`)
+        .then(res=>{
+            setRecommendations(res.data)
+            
+        })
+    },[user?.email])
     return (
         <div>
-            fdds
+            {recommendations.length}
         </div>
     );
 };
